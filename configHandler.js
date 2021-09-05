04087a5f54f0d7d74f458852951ab3168872c6e1
Andrewiski <adevries@digitalexample.com>
2021-09-05 02:21:02 -0400

    config create fix

--- configHandler.js
+++ configHandler.js
@@ -50,8 +50,18 @@ var ConfigHandler = function (options) {
 
         var configFileSettings = {};
         try {
-            var strConfig = fs.readFileSync(path.join(__dirname, self.objOptions.configFileName));
-            configFileSettings = JSON.parse(strConfig);
+            if(fs.existsSync(path.join(__dirname, self.objOptions.configFileName)) === false){
+                console.log("info", "Config File Missing Creating with Defaults", ex);
+                try {
+                    //if we Can't read the config its a new config or a broken config so we create it using the defaults
+                    fs.writeFileSync(path.join(__dirname, self.objOptions.configFileName), JSON.stringify(defaultConfig, null, 2));
+                } catch (ex) {
+                    console.log("error", "Error Creating New Config File just using defaults", ex);
+                }
+            }else{
+                var strConfig = fs.readFileSync(path.join(__dirname, self.objOptions.configFileName));
+                configFileSettings = JSON.parse(strConfig);
+            }
         } catch (ex) {
             //This needs to stay Console.log as writetolog will not function as no config
             try {
